a240397059b6d097b42dbb394c1770214117d9c2
akadwa <kadazh123@gmail.com>
2023-07-24 17:51:43 +0200

    Add HTML, CSS + basic maths functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// Variables
+let num1;
+let num2;
+let operator;
+
+
+// Basic Math Operation Functions
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case "add":
+      return add(num1, num2);
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+  }
+}
